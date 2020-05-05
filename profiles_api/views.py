634c2be9dcc0a3b8202c09db3bce5c87a3620670
mightnent <mightnent@gmail.com>
2020-05-05 16:54:57 +0800

    add filters

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-# from rest_framework import filters
+from rest_framework import filters
 # from rest_framework.authtoken.views import ObtainAuthToken
 # from rest_framework.settings import api_settings
 # from rest_framework.permissions import IsAuthenticated
@@ -99,3 +99,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     #these var names are not anyhow assigned. they carry meaning
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields =('name','email',)
